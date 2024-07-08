3fe91ada7191373bd29de801c997f813497cc78e
Patrick Alvin Alcala <patrick_alv@yahoo.com>
2024-07-08 08:27:50 +0800

    update

---
+++ src/stores/rsakey.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useRSAKey = defineStore('rsakey', {
+  state: () => {
+    return {
+      private: '',
+      public: ',
+    }
+  },
+
+  getters: {
+    privatekey: (state) => state.private,
+    publickey: (state) => state.public,
+  },
+})
