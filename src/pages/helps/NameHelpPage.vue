61f4717d9b92ff90c30ea1eb3abd7592277c4441
Patrick Alvin Alcala <45809622+PatAlcala9@users.noreply.github.com>
2024-08-27 20:36:19 +0800

    added images

--- src/pages/helps/NameHelpPage.vue
+++ src/pages/helps/NameHelpPage.vue
@@ -9,9 +9,21 @@ q-page.page(padding)
     section.step.fit.column.wrap.justify-center.items-center.content-center
       span.details Type the Name you want to search, please be specific
       picture
-        source(srcset="../../assets/images/mobile/app-mobile1.avif" type="image/avif")
-        img.image(src="../../assets/images/mobile/app-mobile1.webp" type="image/webp" alt="Application search image")
+        source(srcset="../../assets/images/mobile/name-mobile1.avif" type="image/avif")
+        img.image(src="../../assets/images/mobile/name-mobile1.webp" type="image/webp" alt="Name search image")
 
+    section.step.fit.column.wrap.justify-center.items-center.content-center
+      span.details Wait for it to find the list
+      picture
+        source(srcset="../../assets/images/mobile/name-mobile1.avif" type="image/avif")
+        img.image(src="../../assets/images/mobile/name-mobile1.webp" type="image/webp" alt="Name search image")
+
+    section.step.fit.column.wrap.justify-center.items-center.content-center
+      span.details Click the button to get Application Number
+      picture
+        source(srcset="../../assets/images/mobile/name-mobile1.avif" type="image/avif")
+        img.image(src="../../assets/images/mobile/name-mobile1.webp" type="image/webp" alt="Name search image")
+        
     //- section(class="step fit column wrap justify-center items-center content-center")
     //-   span(class="details") Wait to finish downloading
     //-   span(class="details-extra") as seen below
@@ -27,7 +39,7 @@ q-page.page(padding)
     //-   span(class="details-extra") as seen below
     //-   img(class="image" src="../../assets/images/mobile/application_search_mobile4.jpg" alt="application_search_image")
 
-    q-btn.button(rounded label="Back" @click="returnHome")
+    q-btn.button-back2(rounded label="Back" @click="returnHome")
 
   //- div.fit.column.wrap.justify-center.items-center.content-center(v-else-if="screenWidth > 500 && screenWidth <= 1000" )
   //-   //- h3(class="main-title2 center-text") Application Status Guide
@@ -57,37 +69,56 @@ q-page.page(padding)
   div.fit.column.wrap.justify-center.items-center.content-center(v-else)
     h3.main-title2.center-text Name Exist Guide
 
-    section.step.fit.column.wrap.justify-center.items-center.content-center
-      span.details Type the Name
-      span.details-extra as demonstated below
-      img.image(src="../../assets/images/pc/name_search.jpg" alt="application_search_image")
+    section.step
+      div.grid
+        div.grid-right
+          span.details Type your Name
+          span.details Please note, avoid generic search
+        //- span.details-extra as demonstated below
+        //- img.image(src="../../assets/images/pc/app1.webp" alt="application_search_image")
+        div.grid-left
+          picture.fit.column.wrap.justify-center.items-center.content-center
+            source(srcset="../../assets/images/pc/name1.avif" type="image/avif")
+            img.image(src="../../assets/images/pc/name1.webp" type="image/webp" alt="Name search image")
 
-    section.step.fit.column.wrap.justify-center.items-center.content-center
-      span.details Accounts containing that name will be display
-      span.details-extra as demonstated below
-      img.image(src="../../assets/images/pc/name_search2.jpg" alt="application_search_image")
+    section.step
+      div.grid
+        div.grid-left
+          span.details Wait for it to find the list
+          //- span.details-extra as demonstated below
+          //- img.image(src="../../assets/images/pc/app2.webp" alt="application_search_image")
+        div.grid-right
+          picture.fit.column.wrap.justify-center.items-center.content-center
+            source(srcset="../../assets/images/pc/name2.avif" type="image/avif")
+            img.image(src="../../assets/images/pc/name2.webp" type="image/webp" alt="Name search image")
 
-    section.step.fit.column.wrap.justify-center.items-center.content-center
-      span.details Use the search above for more specific
-      span.details-extra as demonstated below
-      img.image(src="../../assets/images/pc/name_search3.jpg" alt="application_search_image")
+    section.step
+      div.grid
+        div.grid-right
+          span.details Click the button to get Application Number
+          //- span.details-extra as demonstated below
+          //- img.image(src="../../assets/images/pc/app2.webp" alt="application_search_image")
+        div.grid-left
+          picture.fit.column.wrap.justify-center.items-center.content-center
+            source(srcset="../../assets/images/pc/name3.avif" type="image/avif")
+            img.image(src="../../assets/images/pc/name3.webp" type="image/webp" alt="Name search image")
 
-    section.step.fit.column.wrap.justify-center.items-center.content-center
-      span.details Click the Check button to check current status
-      span.details-extra as demonstated below
-      img.image(src="../../assets/images/pc/name_search4.jpg" alt="application_search_image")
+    //- section.step.fit.column.wrap.justify-center.items-center.content-center
+    //-   span.details Click the Check button to check current status
+    //-   span.details-extra as demonstated below
+    //-   img.image(src="../../assets/images/pc/name_search4.jpg" alt="application_search_image")
 
-    section.step.fit.column.wrap.justify-center.items-center.content-center
-      span.details Building Status Displayed
-      span.details-extra as seen below
-      img.image(src="../../assets/images/pc/name_search5.jpg" alt="application_search_image")
+    //- section.step.fit.column.wrap.justify-center.items-center.content-center
+    //-   span.details Building Status Displayed
+    //-   span.details-extra as seen below
+    //-   img.image(src="../../assets/images/pc/name_search5.jpg" alt="application_search_image")
 
-    section.step.fit.column.wrap.justify-center.items-center.content-center
-      span.details Occupancy Status Displayed
-      span.details-extra as seen below
-      img.image(src="../../assets/images/pc/name_search6.jpg" alt="application_search_image")
+    //- section.step.fit.column.wrap.justify-center.items-center.content-center
+    //-   span.details Occupancy Status Displayed
+    //-   span.details-extra as seen below
+    //-   img.image(src="../../assets/images/pc/name_search6.jpg" alt="application_search_image")
 
-    q-btn.button(rounded label="Back" @click="returnHome")
+    q-btn.button-back2(rounded label="Back" @click="returnHome")
 
   //- div(v-if="$q.screen.width <= 1000")
   //-   div(class="full-width column no-wrap justify-center items-center content-start")
@@ -129,47 +160,81 @@ const onResize = () => {
 
 <style lang="sass" scoped>
 .main-title2
-  font-family: "PoppinsBold"
-  font-size: 2.3rem
-  margin-bottom: 2rem
+  font-size: 1.6rem
+  margin: 2rem
 
 .step
   margin-bottom: 1.5rem
 
 .details
-  font-family: "PoppinsBold"
-  color: #ffd56b
-  font-weight: bold
-  font-size: 1.1rem
+  font-family: "Roboto"
+  color: $text
+  font-size: 1rem
+  margin: 0 0 1rem 0
 
-.details-extra
-  font-family: "Poppins"
-  color: rgba(255, 213, 107, 0.9)
+// .details-extra
+//   font-family: "Poppins"
+//   color: rgba(255, 213, 107, 0.9)
 
 .image
-  box-shadow: -12px 11px 16px -5px rgba(13, 5, 100, 0.45)
-  width: 80%
+  // box-shadow: -12px 11px 16px -5px rgba(255, 255, 255, 0.45)
+  width: 55vw
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2), 0 6px 10px rgba(0, 0, 0, 0.1)
+  border: 1px solid #263240
+  border-radius: 2rem
 
 .button
   width: 90%
   margin-bottom: 1rem
 
-@media screen and (min-width 400px)
+.grid
+  display: flex
+  flex-direction: column
+  flex-wrap: wrap
+  justify-content: center
+  align-items: center
+  align-content: center
+
+@media screen and (min-width: 1023px)
   .main-title2
     font-size: 2rem
-
-  .image
-    width: 50%
+    margin-bottom: 2rem
 
   .details
-    font-size: 1.5rem
+    font-size: 1.2rem
 
-  .button
-    margin: 2rem
-    width: 90%
+  .image
+    width: 40%
 
-@media screen and (min-width: 900px)
-  .button
-    width: 250px
-    margin: 2rem 0 0 0
+  .grid
+    display: grid
+    grid-template-columns: 1fr 1fr
+    grid-template-rows: 1fr
+    gap: 0px 0px
+    grid-template-areas: "grid-left grid-right"
+
+  .grid-left
+    grid-area: grid-left
+    display: flex
+    flex-direction: column
+    flex-wrap: wrap
+    justify-content: center
+    align-items: center
+    align-content: center
+
+  .grid-right
+    grid-area: grid-right
+    display: flex
+    flex-direction: column
+    flex-wrap: wrap
+    justify-content: center
+    align-items: center
+    align-content: center
+
+  .image
+    // box-shadow: -12px 11px 16px -5px rgba(255, 255, 255, 0.45)
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2), 0 6px 10px rgba(0, 0, 0, 0.1)
+    border: 1px solid #263240
+    border-radius: 2rem
+    width: 40vw
 </style>
