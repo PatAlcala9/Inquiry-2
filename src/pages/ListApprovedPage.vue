a4b4c2c9c4b3a7390519cfb2698cb5ce3478b1c4
Patrick Alvin Alcala <45809622+PatAlcala9@users.noreply.github.com>
2024-09-19 17:22:55 +0800

    fix display

--- src/pages/ListApprovedPage.vue
+++ src/pages/ListApprovedPage.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
 
 q-page.page(padding)
-  section.full-width.column.wrap.justify-center.items-center.content-center
-    span.title List of Approved {{ _division.getValue }} Permits
-    span.subheader on
-    span.header {{ properDate }}
+  div.title-area
+    span.main List of Approved {{ _division.getValue }} Permits
+    span.submain on
+    span.date {{ properDate }}
 
-    section.fit.row.wrap.justify-center.items-center.content-center
-      div.summary--count2
-        span.label Total Count
-        span.content {{ rowCount }}
+  div.grid
+    section.bar-area
+      section
+        div.summary--count2
+          span.label Total Count
+          span.content {{ rowCount }}
 
-
-
-    section(v-if="quasar.screen.width <= 767")
-      div.back-button.full-width.column.wrap.justify-center.items-center.content-center
+    section.button-area(v-if="quasar.screen.width <= 767")
+      divfull-width.column.wrap.justify-center.items-center.content-center
         BackButton(text="Back" @click="gotoHome")
 
       div.table-data-mobile.fit.column.wrap.justify-center.items-center.content-center.text-align(v-for="(item, index) in _tabledata.getTable.result" :key="item")
@@ -39,8 +39,8 @@ q-page.page(padding)
             td {{_tabledata.getTable.result6[index]}}
             td {{date.formatDate(_tabledata.getTable.result7[index], 'MMMM DD, YYYY')}}
 
-      div.back-button.full-width.column.wrap.justify-center.items-center.content-center
-        BackButton(text="Back" @click="gotoHome")
+    div.button-area
+      BackButton(text="Back" @click="gotoHome")
 </template>
 
 <script>
@@ -150,6 +150,37 @@ const gotoHome = () => {
 </script>
 
 <style lang="sass" scoped>
+.title-area
+  display: flex
+  flex-direction: column
+  flex-wrap: wrap
+  justify-content: center
+  align-items: center
+  align-content: center
+  padding: 1rem 0 0 0
+  color: $text
+  font-family: 'Roboto'
+  font-weight: bold
+
+  & .main
+    font-size: 1.2rem
+
+  & .submain
+    font-size: 1.1rem
+
+  & .date
+    font-size: 1.4rem
+    margin: 0 0 2rem 0
+
+.grid
+  display: flex
+  flex-direction: column
+  flex-wrap: wrap
+  justify-content: center
+  align-items: center
+  align-content: center
+  text-align: center
+
 .subheader
   font-family: 'Roboto'
   font-weight: bold
@@ -167,7 +198,7 @@ const gotoHome = () => {
   padding: 0 0 1rem 0
 
 .table-contain
-  height: 500px
+  height: 800px
   overflow-y: auto
 
 .table-custom thead
@@ -184,6 +215,7 @@ const gotoHome = () => {
   padding: 1rem
   font-size: 1rem
   border-bottom: 1px solid $text
+  text-align: left
 
 // .summary--count
 //   display: flex
@@ -247,6 +279,50 @@ const gotoHome = () => {
 
 
 @media screen and (min-width: 1024px)
+  .title-area
+    display: flex
+    flex-direction: row
+    flex-wrap: wrap
+    justify-content: flex-start
+    align-items: center
+    align-content: center
+    gap: 1rem
+    margin: 0 0 2rem 1rem
+
+    & .main
+      font-size: 2rem
+
+    & .submain
+      font-size: 1.8rem
+
+    & .date
+      font-size: 2rem
+      margin: 0
+
+  .grid
+    display: grid
+    grid-template-columns: 0.2fr 1.8fr
+    grid-template-rows: 1.7fr 0.3fr
+    gap: 0px 1rem
+    grid-template-areas: "bar-area table-area" "bar-area button-area"
+    max-width: 1920px
+
+  .bar-area
+    grid-area: bar-area
+    display: flex
+    justify-self: center
+    align-self: start
+
+  .table-area
+    grid-area: table-area
+    justify-self: start
+    align-self: start
+
+  .button-area
+    grid-area: button-area
+    justify-self: start
+    align-self: start
+
   .subheader
     font-family: 'Roboto'
     font-weight: bold
